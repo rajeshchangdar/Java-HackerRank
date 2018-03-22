9178d993553874bf77728641e460865eeb5ed2b0
rajeshchangdar <it1217@cemk.ac.in>
2018-03-23 03:39:04 +0530

    Add files via upload

---
+++ Solution.java
@@ -0,0 +1,71 @@
+package com.probsol;
+
+import java.util.*;
+import java.io.*;
+
+class Solution
+{
+	
+	public void calculate(int a,int b,int n)
+	{
+		int i,val=a;
+		int s[]=new int[n];
+		for(i=0;i<n;i++)
+		{
+			val=(int) (val+(Math.pow(2,i)*b));
+			
+			s[i]=s[i]+val;
+		}		
+		for(i=0;i<n;i++)
+		{
+			System.out.print(" "+s[i]+" ");
+		}
+		
+		System.out.print("\n");
+	}
+	
+    public static void main(String []argh) throws IOException, NumberFormatException
+    {
+    	
+    	/*
+    	DataInputStream dis = new DataInputStream(System.in);
+    	String s = dis.readLine();
+    	String arr [] = s.split(" ");
+    	int len = arr.length;
+    	int iArr [] = new int [len];
+    	
+    	for(int y=0; y<arr.length; y++){
+    		iArr[y] = Integer.parseInt(arr[y]);
+    	}
+    	
+    	for(int y=0; y<arr.length; y++){
+    	System.out.print(iArr [y]+" ");
+    	}*/
+    	
+    	Solution sol=new Solution();
+    	Scanner in = new Scanner(System.in);
+        int t=in.nextInt();
+        int arr[][]=new int[t][3];
+        
+        for(int i=0;i<t;i++)
+        {
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+            
+            arr[i][0]=a;
+            arr[i][1]=b;
+            arr[i][2]=n;
+         }
+        
+        for(int i=0;i<t;i++)
+        {
+        	sol.calculate(arr[i][0],arr[i][1],arr[i][2]);
+        }
+        
+        in.close();
+        
+    	
+    	
+    }
+}
